22a9b7fc28dea6cc43b196e01b45587dd6f4313d
DESKTOP-3C35ERR\kitew <kitewinpsh@naver.com>
2021-08-26 22:24:05 +0900

    데이터베이스 연결 설정 변경 및 쿼리 로그 파일 추가

--- src/main/java/com/example/SpringExampleApplication.java
+++ src/main/java/com/example/SpringExampleApplication.java
@@ -1,34 +1,15 @@
 package com.example;
 
-import javax.sql.DataSource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 //@ComponentScan(basePackages="com") base package를 변경하는 anotation
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-@MapperScan(basePackages = "com.example.*") // mapper scan 추가!!!
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) // 데이터베이스 설정안함
+//@MapperScan(basePackages = "com.example.*") // mapper scan 추가!!!
 @SpringBootApplication
 public class SpringExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringExampleApplication.class, args);
-	}
-
-	@Bean // Spring bean을 의미
-	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource);
-
-		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
-		sessionFactory.setMapperLocations(res);
-
-		return sessionFactory.getObject();
 	}
 }
---
+++ src/main/java/com/example/config/DatabaseConfig.java
@@ -0,0 +1,27 @@
+package com.example.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration // 설정을 의미 - 서버를 시작할때 읽어들인다.
+@MapperScan(basePackages="com.example.*") // interface(mapper)가 있는 패키지 경로
+public class DatabaseConfig {
+
+	@Bean // Spring bean을 의미
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+		sessionFactory.setMapperLocations(res);
+
+		return sessionFactory.getObject();
+	}
+}
