bfdebf149e35d6f23f347eb557862e8e9eba9b4d
DESKTOP-3C35ERR\kitew <kitewinpsh@naver.com>
2021-08-27 20:49:16 +0900

    lesson03 ex01 추가

---
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -0,0 +1,18 @@
+package com.example.lesson03.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.lesson03.dao.ReviewDAO;
+import com.example.lesson03.model.Review;
+
+@Service
+public class ReviewBO {
+	
+	@Autowired
+	private ReviewDAO reviewDAO;
+	
+	public Review getReviewById(int id) {
+		return reviewDAO.selectReviewById(id);
+	}
+}
