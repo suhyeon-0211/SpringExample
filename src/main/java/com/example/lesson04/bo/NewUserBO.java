ad41e3081796568293ece7dff3ebcbe5214d3e11
DESKTOP-3C35ERR\kitew <kitewinpsh@naver.com>
2021-08-31 21:57:52 +0900

    lesson04 ex01 파일 추가

---
+++ src/main/java/com/example/lesson04/bo/NewUserBO.java
@@ -0,0 +1,18 @@
+package com.example.lesson04.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.lesson04.dao.NewUserDAO;
+import com.example.lesson04.model.NewUser;
+
+@Service
+public class NewUserBO {
+
+	@Autowired
+	private NewUserDAO newUserDAO;
+	
+	public void addNewUser(NewUser newUser) {
+		newUserDAO.insertNewUser(newUser);
+	}
+}
